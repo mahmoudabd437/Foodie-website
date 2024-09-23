fe4841d3865f161e02b62a92c62f9a1b2de4a090
mahmoudabd437 <141342718+mahmoudabd437@users.noreply.github.com>
2024-09-23 22:48:26 +0300

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,44 +1,58 @@
-let arrowUp = document.querySelector('.arrow-up');
-let nav = document.querySelector('nav');
-let span = document.querySelector('.span');
-let linksActive = document.querySelector('.links-active');
-let close = document.querySelector('.close');
+let arrowUp = document.querySelector(".arrow-up");
+let nav = document.querySelector("nav");
+let span = document.querySelector(".span");
+let linksActive = document.querySelector(".links-active");
+let close = document.querySelector(".close");
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 0) {
-        arrowUp.classList.add("active");
-        nav.classList.add("active");
-    } else {
-        arrowUp.classList.remove("active");
-        nav.classList.remove("active");
-    }
-})
+  let scr = document.documentElement.scrollTop;
+  el.style.width = (scr / height) * 100 + "%";
+});
 
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    arrowUp.classList.add("active");
+    nav.classList.add("active");
+  } else {
+    arrowUp.classList.remove("active");
+    nav.classList.remove("active");
+  }
+});
 
-arrowUp.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-})
+arrowUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-span.addEventListener('click', () => {
-    linksActive.classList.toggle('active');
-    span.classList.toggle('active');
+span.addEventListener("click", () => {
+  linksActive.classList.toggle("active");
+  span.classList.toggle("active");
+});
 
-})
-
-let arr = [".has-reveal", ".menu", ".about", ".promo", ".menu-adv", ".banner", ".blog"];
+let arr = [
+  ".has-reveal",
+  ".menu",
+  ".about",
+  ".promo",
+  ".menu-adv",
+  ".banner",
+  ".blog",
+];
 
 for (let i = 0; i < arr.length; i++) {
-    ScrollReveal().reveal(arr[i], {
-        duration: 700,
-        origin: 'bottom',
-        distance: '10px',
-        delay: 100,
-        easing: 'ease',
-        // reset: true
-    });
+  ScrollReveal().reveal(arr[i], {
+    duration: 700,
+    origin: "bottom",
+    distance: "10px",
+    delay: 100,
+    easing: "ease",
+    reset: false,
+  });
 }
 
 const deliveryBoy = document.querySelector(".cycle");
@@ -47,20 +61,18 @@ let deliveryBoyMove = -80;
 let lastScrollPos = 0;
 
 window.addEventListener("scroll", function () {
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
 
-    let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
 
-    if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
-        let activeScrollPos = window.scrollY;
-
-        if (lastScrollPos < activeScrollPos) {
-            deliveryBoyMove += 1;
-        } else {
-            deliveryBoyMove -= 1;
-        }
-
-        lastScrollPos = activeScrollPos;
-        deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
     }
 
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
 });
